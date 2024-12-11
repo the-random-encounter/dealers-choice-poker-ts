bab71bb757a55634bd4872f068049529afb25b9d
the-random-encounter <31645891+the-random-encounter@users.noreply.github.com>
2024-12-11 16:24:13 -0600

    Updated shuffling algorithm

--- src/classes/Deck.ts
+++ src/classes/Deck.ts
@@ -7,14 +7,18 @@ export default class Deck {
   cards: Card[];
 
   constructor(autoShuffle?: boolean) {
+    
+    // Construct new deck, and shuffle if shuffle flag is set true
     if (autoShuffle === true) {
       this.cards = this.generateDeck();
       this.shuffleDeck();
+    // If no shuffle flag is set, just generate new deck in order
     } else {
       this.cards = this.generateDeck();
     }
   }
 
+  // Implement iterator for deck, so that it can be looped through easily
   [Symbol.iterator]() {
         let index = 0;
         const cards = this.cards;
@@ -30,11 +34,14 @@ export default class Deck {
         };
     }
 
-  private generateDeck() {
+
+  private generateDeck(): Card[] {
     const suits: Suit[] = ['hearts', 'diamonds', 'clubs', 'spades'];
     const ranks: Rank[] = ['two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace'];
 
     let cardArray: Card[] = [];
+
+    // Generate deck contents in order, so that the deck is always the same
     for (const suit of suits) {
       for (const rank of ranks) {
         const card = new Card(suit, rank);
@@ -44,16 +51,12 @@ export default class Deck {
     return cardArray;
   }
 
-  shuffleDeck() {
-    // this.cards.sort(() => Math.random() - 0.5);
-
-    let i, j, tempi, tempj;
-    for (i = 0; i < this.cards.length; i += 1) {
-        j = Math.floor(Math.random() * (i + 1));
-        tempi = this.cards[i];
-        tempj = this.cards[j];
-        this.cards[i] = tempj;
-        this.cards[j] = tempi;
+  shuffleDeck(): void {
+    // Shuffle deck using Fisher-Yates
+    for (let i = this.cards.length; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
     }
+    return;
   }
 }
