0563daee0d9b9d30202ec37778fba52147a740d7
the-random-encounter <31645891+the-random-encounter@users.noreply.github.com>
2024-12-16 00:07:00 -0600

    Added dealNext(), dealCards(0, clearTable(), addPlayer(), prepareToDeal()

--- src/classes/Table.ts
+++ src/classes/Table.ts
@@ -2,7 +2,9 @@ import Player from "./Player";
 import Deck from './Deck';
 import Card from "./Card";
 import Hand from './Hand';
+import Phaser from 'phaser';
 import * as funcs from '../utils/functions';
+import { GameConfig, Board } from '../utils/types';
 
 export default class Table {
 
@@ -15,15 +17,18 @@ export default class Table {
   pot: number = 0;
   sidePots?: number[];
   deck: Deck;
+  board: Board = { flops: [], turns: [], rivers: [] };
+  gameConfig: GameConfig;
+  burnPile: Card[] = [];
+  dealOrder: Array<Array<Player | string>>;
 
-  constructor(tableName: string) {
+  constructor(tableName: string, config: GameConfig) {
     this.tableName = tableName;
     this.tableID = this.generateTableID();
+    this.gameConfig = config;
+    this.deck = new Deck(true);
 
-    this.dealer = this.players[0];
-    this.players[0].currentDealer = true;
-    this.players[0].currentTableID = this.tableID;
-    this.players[0].table = this;
+
     if (this.players.length > 2) {
       this.smallBlind = this.players[1];
       this.bigBlind = this.players[2];
@@ -48,5 +53,116 @@ export default class Table {
   // Placeholder Table ID generation until Multiplayer functionality is available
   private generateTableID(): number {    
     return Math.floor(Math.random() * 1000);
+  }
+
+  startRound(): void {
+
+  }
+
+  prepareToDeal(): void {
+    let dealOrder: Array<Array<Player | string>> = [];
+    
+    let playerCards: Player[] = [];
+    for (let i = 0; i < this.gameConfig.cardsPerPlayer; i++) {
+      for (const player of this.players)
+        playerCards.push(player);
+    }
+
+    dealOrder.push(playerCards);
+    dealOrder.push(['burn']);
+
+    for (let i = 0; i < this.gameConfig.numberOfFlops; i++) {
+      dealOrder.push(['flop']);
+      dealOrder.push(['burn']);
+    }
+
+
+    for (let i = 0; i < this.gameConfig.numberOfTurns; i++) {
+      dealOrder.push(['turn']);
+      dealOrder.push(['burn']);
+    }
+
+    for (let i = 0; i < this.gameConfig.numberOfRivers; i++) {
+      dealOrder.push(['river']);
+    }
+  }
+
+  dealNext(): void {
+    let nextDeal = this.dealOrder.shift();
+    if (nextDeal) {
+      if (nextDeal[0] === 'burn')
+        this.burnPile.push(this.deck.draw());
+      else if (nextDeal[0] === 'flop') {
+        let flop: Card[] = [];
+        for (let i = 0; i < 3; i++)
+          flop.push(this.deck.draw());
+        this.board.flops.push(flop);
+      } else if (nextDeal[0] === 'turn')
+        this.board.turns.push(this.deck.draw());
+      else if (nextDeal[0] === 'river')
+        this.board.rivers.push(this.deck.draw());
+      else {
+        const player = nextDeal[0] as Player;
+        if (!player.currentHand)
+          player.currentHand = new Hand([]);
+        player.currentHand.addCard(this.deck.draw());
+      }
+    }
+  }
+  dealCards(): void {
+    // Reset and shuffle deck
+    this.deck.regenerateDeck();
+    this.deck.shuffleDeck();
+
+    // Deal cards to players
+    for (let i = 0; i < this.gameConfig.cardsPerPlayer; i++) {
+      for (const player of this.players) {
+        const card = this.deck.draw();
+        if (!player.currentHand) {
+          player.currentHand = new Hand([]);
+        }
+        player.currentHand.addCard(card);
+      }
+    }
+
+    this.burnPile.push(this.deck.draw());
+
+    // Deal flops
+    for (let i = 0; i < this.gameConfig.numberOfFlops; i++) {
+      const flop: Card[] = [];
+      for (let j = 0; j < 3; j++) {
+        flop.push(this.deck.draw());
+      }
+      this.board.flops.push(flop);
+    }
+
+    this.burnPile.push(this.deck.draw());
+
+    // Deal turns
+    for (let i = 0; i < this.gameConfig.numberOfTurns; i++) {
+      this.board.turns.push(this.deck.draw());
+    }
+
+    this.burnPile.push(this.deck.draw());
+
+    // Deal rivers
+    for (let i = 0; i < this.gameConfig.numberOfRivers; i++) {
+      this.board.rivers.push(this.deck.draw());
+    }
+  }
+
+  clearTable(): void {
+    // Clear all hands
+    for (const player of this.players) {
+      player.currentHand = undefined;
+    }
+
+    // Clear board
+    this.board = { flops: [], turns: [], rivers: [] };
+  }
+
+  addPlayer(player: Player): void {
+    this.players.push(player);
+    player.currentTableID = this.tableID;
   }
 }
