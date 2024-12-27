b7e2b9f292cb1c88347095b8b49e2a17893a038e
the-random-encounter <31645891+the-random-encounter@users.noreply.github.com>
2024-12-26 21:44:31 -0600

    Relocated webpack folder back to original location for unified server/client project

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -1,28 +1,18 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
 const webpack = require("webpack");
 
-const line = "---------------------------------------------------------";
-const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
-process.stdout.write(`${line}\n${msg}\n${line}\n`);
-
 module.exports = {
-    mode: "production",
-    entry: "./src/main.ts",
+    mode: "development",
+    devtool: "eval-source-map",
+    entry: "./client/src/main.ts",
     output: {
         path: path.resolve(process.cwd(), 'dist'),
-        filename: "./bundle.min.js"
+        filename: "bundle.min.js"
     },
     resolve: {
         extensions: [".ts", ".js", ".json"]
-    },
-    devtool: false,
-    performance: {
-        maxEntrypointSize: 2500000,
-        maxAssetSize: 1200000
     },
     module: {
         rules: [
@@ -49,38 +39,22 @@ module.exports = {
             }
         ]
     },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    output: {
-                        comments: false
-                    }
-                }
-            })
-        ]
-    },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [path.join(__dirname, "dist/**/*")]
+        }),
         new webpack.DefinePlugin({
             "typeof CANVAS_RENDERER": JSON.stringify(true),
             "typeof WEBGL_RENDERER": JSON.stringify(true),
-            "typeof WEBGL_DEBUG": JSON.stringify(false),
-            "typeof EXPERIMENTAL": JSON.stringify(false),
+            "typeof WEBGL_DEBUG": JSON.stringify(true),
+            "typeof EXPERIMENTAL": JSON.stringify(true),
             "typeof PLUGIN_3D": JSON.stringify(false),
             "typeof PLUGIN_CAMERA3D": JSON.stringify(false),
             "typeof PLUGIN_FBINSTANT": JSON.stringify(false),
             "typeof FEATURE_SOUND": JSON.stringify(true)
         }),
         new HtmlWebpackPlugin({
-            template: "./index.html"
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: 'public/assets', to: 'assets' },
-                { from: 'public/favicon.png', to: 'favicon.png' },
-                { from: 'public/style.css', to: 'style.css' }
-            ],
-        }),
+            template: "./client/index.html"
+        })
     ]
 };
